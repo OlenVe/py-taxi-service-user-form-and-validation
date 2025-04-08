e717e52819ba9723f0db31a530231a3760800b31
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-04-08 17:29:38 +0200

    fixed_validationError_v2

--- taxi/forms.py
+++ taxi/forms.py
@@ -6,26 +6,30 @@ from django.core.exceptions import ValidationError
 from taxi.models import Driver, Car
 
 
+def validate_license_number(license_number):
+    if Driver.objects.filter(license_number=license_number).exists():
+        raise ValidationError("This license number is already in use.")
+    if len(license_number) != 8:
+        raise ValidationError("This license number must have 8 characters.")
+    if (not license_number[:3].isalpha() or not license_number[:3].isupper()
+            or not license_number[3:].isdigit()):
+        raise ValidationError("This license number must starts with "
+                              "3 letters and ends with 5 numbers.")
+    return license_number
+
+
 class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Driver
         fields = UserCreationForm.Meta.fields + (
             "license_number",
             "first_name",
-            "last_name",)
+            "last_name",
+        )
 
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
-        if Driver.objects.filter(license_number=license_number).exists():
-            raise ValidationError("This license number is already in use.")
-        if len(license_number) != 8:
-            raise ValidationError("This license number "
-                                  "must have 8 characters.")
-        if (not license_number[:3].isalpha()
-                or not license_number[3:].isdigit()):
-            raise ValidationError("This license number must starts with "
-                                  "3 letters and ends with 5 numbers.")
-        return license_number
+        return validate_license_number(license_number)
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
@@ -35,16 +39,7 @@ class DriverLicenseUpdateForm(forms.ModelForm):
 
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
-        if Driver.objects.filter(license_number=license_number).exists():
-            raise ValidationError("This license number is already in use.")
-        if len(license_number) != 8:
-            raise ValidationError("This license number "
-                                  "must have 8 characters.")
-        if (not license_number[:3].isalpha()
-                or not license_number[3:].isdigit()):
-            raise ValidationError("This license number must starts with "
-                                  "3 letters and ends with 5 numbers.")
-        return license_number
+        return validate_license_number(license_number)
 
 
 class CarForm(forms.ModelForm):
